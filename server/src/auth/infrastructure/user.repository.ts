49f1f26f241200e22cf98e8d26743d8714af56d1
Vu Hoang Minh Nhat <90960555+vhoangminhnhat@users.noreply.github.com>
2026-02-25 11:54:04 +0700

    Chore: prisma settings for server-side adjustment

--- server/src/auth/infrastructure/user.repository.ts
+++ server/src/auth/infrastructure/user.repository.ts
@@ -1,20 +1,19 @@
+import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { User } from '../domain/entity/user.entity';
-import * as bcrypt from 'bcrypt';
 import { IUserRepository } from '../domain/interface/user.repository.interface';
 
 @Injectable()
 export class UserRepository implements IUserRepository {
-  private users: User[] = [];
+  constructor(private readonly prisma: PrismaService) {}
 
-  constructor() {
-    const hash = bcrypt.hashSync('123456', 10);
-    this.users.push(
-      new User('1', 'admin@example.com', hash, 'admin'),
-    );
-  }
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
 
-  async findByEmail(email: string): Promise<User | null> {
-    return this.users.find((u) => u.email === email) ?? null;
+    if (!user) return null;
+
+    return new User(user.id, user.email, user.password, user.role);
   }
 }
