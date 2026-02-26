fd9f6011984530beee9b24ccc1cbabfce2b20742
Vu Hoang Minh Nhat <90960555+vhoangminhnhat@users.noreply.github.com>
2026-02-26 11:22:50 +0700

    Chore: removed unused dependency

--- server/src/auth/domain/interface/user.repository.interface.ts
+++ server/src/auth/domain/interface/user.repository.interface.ts
@@ -1,7 +1,6 @@
-import { SignUpDto } from "@/auth/dto/signUp.dto";
-import { User } from "../entity/user.entity";
+import { User } from '../entity/user.entity';
 
 export interface IUserRepository {
   findByEmail(email: string): Promise<User | null>;
-  checkExisted(userInfo: SignUpDto): Promise<User | null>;
+  createUser(signUpInfo: User): Promise<User>;
 }
